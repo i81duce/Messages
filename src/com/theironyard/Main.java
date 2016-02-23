441cb3b48b7858509ee3b543571f1ee62216df33
KevinBozic <bozic.kevin@gmail.com>
2016-02-23 14:15:10 -0500

    Added saved user

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,31 +1,27 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
-    static User user;
-
+    static HashMap<String, User> users = new HashMap<>();
 
     public static void main(String[] args) {
         Spark.init();
-        ArrayList<Messages> allMessages = new ArrayList<>();
-
         Spark.get(
                 "/",
                 ((request, response) -> {
+                    User user = getUserFromSession(request.session());
+
                     HashMap m = new HashMap();
                     if (user == null) {
-                        return new ModelAndView(m, "index.html");
+                        return new ModelAndView(m, "login.html");
                     } else {
-                        m.put("name", user.name);
-                        m.put("allMessages", allMessages);
-                        return new ModelAndView(m, "messages.html");
+                        return new ModelAndView(user, "messages.html");
                     }
                 }),
                 new MustacheTemplateEngine()
@@ -35,7 +31,15 @@ public class Main {
                 "/create-user",
                 ((request, response) -> {
                     String name = request.queryParams("loginName");
-                    user = new User(name);
+                    User user = users.get(name);
+                    if (user == null) {
+                        user = new User(name);
+                        users.put(name, user);
+                    }
+
+                    Session session = request.session();
+                    session.attribute("userName", name);
+
                     response.redirect("/");
                     return "";
                 })
@@ -44,13 +48,36 @@ public class Main {
         Spark.post(
                 "/create-message",
                 ((request, response) -> {
+                    User user = getUserFromSession(request.session());
+                    if (user == null) {
+                        Spark.halt(403);
+                    }
+
                     String text = request.queryParams("message");
                     Messages messages = new Messages(text);
-                    messages.id = allMessages.size() + 1;
-                    allMessages.add(messages);
+                    messages.id = user.allMessages.size() + 1;
+
+                    user.allMessages.add(messages);
+
+                    response.redirect("/");
+
+                    return "";
+                })
+        );
+
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    Session session = request.session();
+                    session.invalidate(); //wipes out current session
                     response.redirect("/");
                     return "";
                 })
         );
+    }
+
+    static User getUserFromSession(Session session) {
+        String name = session.attribute("userName");
+        return users.get(name);
     }
 }
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -1,7 +1,10 @@
 package com.theironyard;//Created by KevinBozic on 2/22/16.
 
+import java.util.ArrayList;
+
 public class User {
     String name;
+    ArrayList<Messages> allMessages = new ArrayList<>();
 
     public User(String name) {
         this.name = name;
